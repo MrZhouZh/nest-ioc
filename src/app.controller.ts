643bb7aabfbae657a840d5045a40eb3861fbebd3
MrZhouZh <17600330793@sina.cn>
2023-07-05 22:09:59 +0800

    chore: init ioc

---
+++ src/app.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  // constructor(private readonly appService: AppService) {}
+  // 属性注入
+  @Inject('app_service')
+  private readonly appService: AppService;
+  @Inject('person')
+  private readonly person: { name: string; age: number };
+  @Inject('person2')
+  private readonly person2: { name: string; desc: string };
+  @Inject('person3')
+  private readonly person3: { name: string };
+  @Inject('person4')
+  private readonly person4: { name: string; desc: string };
+  @Inject('person5')
+  private readonly person5: { name: string; age: number };
+  // 或者构造器注入
+  // constructor(
+  //   @Inject('app_service') private readonly appService: AppService,
+  //   @Inject('person') private readonly person: { name: string; age: number },
+  //   @Inject('person2') private readonly person2: { name: string; desc: string },
+  //   @Inject('person3') private readonly person3: { name: string },
+  //   @Inject('person4') private readonly person4: { name: string; desc: string },
+  //   @Inject('person5') private readonly person5: { name: string; age: number },
+  // ) {}
+
+  @Get()
+  getHello(): string {
+    console.log(this.person);
+    console.log(this.person2);
+    console.log(this.person3);
+    console.log(this.person4);
+    console.log(this.person5);
+
+    return this.appService.getHello();
+  }
+}
